c54e3730c8139384452e7ab3419c989b3126e47d
Joseph Tang <162398761+mintetang@users.noreply.github.com>
2025-02-14 21:14:32 +0800

    add production trial 1 for LocalLibrary

--- locallibrary/locallibrary/settings.py
+++ locallibrary/locallibrary/settings.py
@@ -16,15 +16,23 @@ import os # needed by code below
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# Support env variables from .env file if defined
+import os
+from dotenv import load_dotenv
+env_path = load_dotenv(os.path.join(BASE_DIR, '.env'))
+load_dotenv(env_path)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-y7wo7sm3n&q!b)4nqf!&v85_s4o1k0a1vc0c!8b3ekfwr22^c%'
+#SECRET_KEY = 'django-insecure-y7wo7sm3n&q!b)4nqf!&v85_s4o1k0a1vc0c!8b3ekfwr22^c%'
+import os
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-&psk#na5l=p3q8_a+-$4w1f^lt3lx1c@d*p4x$ymm_rn7pwb87')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+#DEBUG = False
+DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
 ALLOWED_HOSTS = []
 
